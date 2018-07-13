02c43859225081efa9f0e085765700ba434de85c
Karsten Krimpmann <karsten.krimpmann@vodafone.com>
2018-07-13 11:05:40 +0200

    added unit tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,34 @@
 "use strict";
+const expect = require('chai').expect
+const user = require('../lib/actions/user.js')
+const messages = require('elasticio-node').messages
 
-const messages = require('elasticio-node').messages;
-const user = require('../lib/actions/user.js');
-
-const msg = messages.newMessageWithBody({email: 'test@test.com', password: 'top_secret' })
-const result = user.process(msg, {});
-console.log("result: %j", result.body)
+describe('existing user', function () {
+  it('should have user_type existing', function () {
+    const existing_user = {email: 'test@test.com', password: 'top_secret' }
+    const result = user.process(messages.newMessageWithBody(existing_user), {})
+    expect(result.body).to.have.property('user').with.have.property('user_type').with.equal('existing')
+    Object.entries(existing_user).forEach((item) => {
+      expect(result.body).to.have.property('user').with.have.property(item[0]).with.equal(item[1])
+    })
+  })
+})
+describe('new user', function () {
+  it('should have user_type filled with all attributes', function () {
+    const new_user = {
+      email: 'test@test.com',
+      password: 'top_secret',
+      first_name: 'Tester',
+      last_name: 'Test',
+      salutation: 'mr',
+      telephone: '+49 211 / 78943158',
+      mobile_phone: '+49 152 034 75896',
+      terms_and_conditions_accepted: true
+    }
+    const result = user.process(messages.newMessageWithBody(new_user), {})
+    expect(result.body).to.have.property('user').with.have.property('user_type').with.equal('new')
+    Object.entries(new_user).forEach((item) => {
+      expect(result.body).to.have.property('user').with.have.property(item[0]).with.equal(item[1])
+    })
+  })
+})
